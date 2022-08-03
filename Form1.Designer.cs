9208edf366666a00898254245c24f166d0933cef
김용주 <dydrkfsla98@naver.com>
2022-08-04 07:54:23 +0900

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            chartArea4.Name = "ChartArea2";
+            this.chart1.ChartAreas.Add(chartArea3);
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(450, 130);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea2";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "ppg";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "ecg";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(598, 384);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "data_read";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 32);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(419, 484);
+            this.listBox1.TabIndex = 30;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1058, 526);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.IO;        //추가
+
+
+namespace project
+{
+    
+    public partial class Form1 : Form
+    {
+        private string stFolderPath1;
+        private System.IO.DirectoryInfo di;
+
+        //그래프
+        int Min = Int32.MaxValue;
+        int Max = Int32.MinValue;
+        int Min2 = Int32.MaxValue;
+        int Max2 = Int32.MinValue;
+        double[] ecg = new double[100000];
+        double[] ppg = new double[100000];
+        private int ecgLength;
+        private int ppgLength;
+        private int Tick = 0;
+        private bool isTimerOn = true;
+        string[] time_table= new string [10000];
+        int[] avg = new int[10000];
+        int index = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            if(isTimerOn)
+            {
+                timer1.Stop(); isTimerOn = false;
+            }
+            else
+            {
+                timer1.Start();
+                isTimerOn = true;
+            }
+        }
+        private void ecgppgRead()
+        {
+            string filename = @"C:\\Users\\dydrk\\source\\repos\\WindowsFormsApp3\\WindowsFormsApp3\\a101.txt";
+            string[] lines = System.IO.File.ReadAllLines(filename);
+            string filename2 = @"C:\\Users\\dydrk\\source\\repos\\WindowsFormsApp3\\WindowsFormsApp3\\PPG.txt";
+            string[] lines2 = System.IO.File.ReadAllLines(filename2);
+            for (int i=0;i<index;i++)
+            {
+                if (avg[i] < Min)
+                    Min = avg[i];
+                if (avg[i] > Max)
+                    Max = avg[i]; 
+            }
+            ecgLength = index;
+            Console.WriteLine("Min = {0}, Max ={1}", Min, Max);
+            i = 0;
+            foreach (string line in lines2)
+            {
+                ppg[i] = Convert.ToDouble(line);
+                if (ppg[i] < Min2) Min2 = Convert.ToInt32(ppg[i]);
+                if (ppg[i] > Max2) Max2 = Convert.ToInt32(ppg[i]);
+                i++;
+            }
+            ppgLength = i;
+            Console.WriteLine("Min2 = {0}, Max2 = {1}", Min2, Max2);
+        }
+      
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            ecgChartSetting(Tick * 10);
+            Tick += 1;
+        }
+        private void ecgChartSetting(int minX)
+        {
+            chart1.ChartAreas["ChartArea1"].AxisX.Minimum = minX;
+            chart1.ChartAreas["ChartArea1"].AxisX.Maximum = minX + 1500;
+            chart1.ChartAreas["ChartArea1"].AxisY.Minimum = Min / 100 * 100;
+            chart1.ChartAreas["ChartArea1"].AxisY.Maximum = Max / 100 * 100;
+            chart1.ChartAreas["ChartArea2"].AxisX.Minimum = minX;
+            chart1.ChartAreas["ChartArea2"].AxisX.Maximum = minX + 1500;
+            chart1.ChartAreas["ChartArea2"].AxisY.Minimum = Min2;
+            chart1.ChartAreas["ChartArea2"].AxisY.Maximum = Max2; ;
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader SR = new StreamReader("C:\\Users\\dydrk\\source\\repos\\project\\project\\dumpState.txt");    //읽어들일 TXT 파일의 경로를 
+                                                                                                                                        //매개변수로 StreamReader 생성
+
+            string line="";                                            //한 줄씩 읽은 후, 그 값을 저장시킬 변수
+            bool check = false;
+            string result = "";                                     //전체 라인을 저장시킬 변수
+            
+            while ((line = SR.ReadLine()) != null)                  //line변수에 SR에서 한줄을 읽은 걸 저장, 읽은 줄이 null이 아닐때까지 반복
+            {
+                if (check == false && line.StartsWith("network statistics:") == true)
+                {
+                    check = true;
+                }
+                else if (check == true && line.StartsWith("packet wakeup events:") != true)
+                {
+                    //result += line;                                     //전체 라인 변수에 그 값을 저장
+                    //result += "\r\n";
+
+                    
+                    //time_table[index] = line.Split(' ')[0];
+                    //string avg_str = line.Split(' ')[3];
+                    int avg_index = line.IndexOf("avg"); int avg_ms_index = line.IndexOf("ms"); int avg_count = avg_ms_index - (avg_index + 4);
+                    //avg[index] = Int32.Parse(avg_str.Substring(avg_index + 3, avg_count));
+                    if (avg_index > 0)
+                    {
+                        //listBox1.Items.Add(line.Substring(avg_index + 4, avg_count));
+                        avg[index] = Int32.Parse(line.Substring(avg_index + 4, avg_count));
+                        index++;
+
+                    }
+                }//표출을 위해서 추가
+                else if (line.StartsWith("packet wakeup events:") == true)
+                {
+                    break;
+                }
+            }
+
+                                    
+            SR.Close();
+        }
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
